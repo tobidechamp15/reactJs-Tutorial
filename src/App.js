22785d2a8e01ec5e73d8956e9d27b459d9a0ad6d
Oluwadare Oluwatobiloba <tobidechamp15@gmail.com>
2023-08-04 15:50:47 +0100

    reactlearning

--- src/App.js
+++ src/App.js
@@ -5,16 +5,7 @@ import FunctionalComp from "./Components/FunctonalComponents";
 import ClassComp from "./Components/ClassComp";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import Click from "./Components/Click";
 
-
-function App() {
-  return (
-    <div className="d-flex flex-column bg-dark text-light p-3">
-      <h1 className="text-danger bg-light p-3">I am a web developer.   My name is Tobiloba</h1>
-      <FunctionalComp></FunctionalComp>
-      <ClassComp></ClassComp>
-    </div>
-  );
-}
 
 export default App;
